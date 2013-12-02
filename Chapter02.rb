da1719bfd7b32536026eaaadbf06eb5aabd7f54f
ShawnKAKU <shawnkuo67@gmail.com>
2013-12-02 14:12:20 +0800

    init

---
+++ Chapter02.rb
@@ -0,0 +1,14 @@
+# Chapter 2
+
+puts "Hours in a year. How many hours are in a year?"
+puts 365 * 24
+
+puts "Minutes in a decade. How many minutes are in a decade?"
+puts 365 * 24 * 60
+
+puts "Your age in seconds. How many seconds old are you?"
+puts 35 * 24 * 60 * 60
+
+puts "If I am 1,160 million seconds old"
+puts "(which I am, though I was somewhere in the 800 millions when I started this book), how old am I?"
+puts 1160000000 / 365 / 24 / 60 / 60
