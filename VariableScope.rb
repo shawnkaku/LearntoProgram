d1b699a62d0b8fb19082ea1eeacb60a88c8c4d91
ShawnKAKU <shawnkuo67@gmail.com>
2013-12-04 22:35:34 +0800

    Add the 2nd question.

--- VariableScope.rb
+++ VariableScope.rb
@@ -1,8 +1,25 @@
-int_var = 5
 
-puts "Before the do/end process. The variable is #{int_var.to_s}."
-1.times do
-  int_var = int_var + 1
-  puts "Inner the do/end process. The variable is #{int_var.to_s}."
+def do_end_process(var)
+  puts "That does not mutate caller. The variable is #{var.to_s}."
+  1.times do
+    var = var + 1
+    puts "That mutates caller. The variable is #{var.to_s}."
+  end
 end
-puts "After the do/end process. The variable is #{int_var.to_s}."
+int_var = 5
+do_end_process(int_var)
+puts "After the process. The variable is #{int_var.to_s}."
+
+
+def outter_process
+  int_var = 5
+  puts "Outter process. The variable is #{int_var.to_s}."
+  def inner_process(var)
+    var = var + 1
+    puts "inner process. The variable is #{var.to_s}."
+  end
+  inner_process(int_var)
+  puts "After the inner process. The variable is #{int_var.to_s}."
+end
+
+outter_process
